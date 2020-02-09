822ad3a9006dff3a5f23a07d26c2319bca840ea2
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-02-09 18:43:30 -0500

    Create Odd.c

---
+++ Odd.c
@@ -0,0 +1,15 @@
+#include< stdio.h>
+#include< conio.h>
+int main()
+{
+  int i=1,n;
+  printf("Enter n : ");
+  scanf("%d",&n);
+  while(i<=n)
+  {
+	   printf("%d\n",i);
+	   i=i+2; 
+	   //increment i by 2
+  }
+  return 0;
+}
