5e11eaa2a03d1e3fe5ebd52224d5dd7f9b689527
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-02-08 21:43:38 -0500

    Create Mergearray.c

---
+++ Mergearray.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+ int i,h=0,k=0;
+ int x[5]={1,2,3,4,5};
+ int y[5]={6,7,8,9,10};
+ int z[10];
+   printf("\n Array 1 elements are:");
+   for (i = 0; i<5; i++) {
+               printf("%d ",x[i]);
+ 
+   }
+   
+   printf("\n Array 2 elements are:");
+   for (i = 0; i<5; i++) {
+               printf("%d ",y[i]);
+ 
+   }
+ 
+   i=0;
+   while(i<10){
+              if(i%2 ==0 )
+              z[i] =x[k++];
+              else
+              z[i]=y[h++];
+              i++;
+              }
+              
+               printf("\n Merged elements are:");
+   for (i = 0; i<10; i++) {
+               printf("%d",z[i]);
+ 
+   }            
+
+    getch();
+
+
+}
