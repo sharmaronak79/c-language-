0ee45ea469b168f5388cf8e8c6642c6378428bf3
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-02-11 19:43:06 -0500

    Create Series.c

---
+++ Series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  int  i , n ;
+
+  printf("Enter n:") ;
+  scanf("%d" , &n) ;
+
+  printf("Required Output:\n") ;
+  printf("x=1!") ;
+  for(i=3 ; i<=2*n-1 ; i=i+2)
+    printf("+%d!" , i) ;
+
+  return 0 ;
+}
