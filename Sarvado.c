4d97e94e7b5a9920a817662ca54e4c966c3f152c
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-02-12 22:40:52 -0500

    Create Sarvado.c

---
+++ Sarvado.c
@@ -0,0 +1,13 @@
+#include< stdio.h>
+#include<conio.h>
+int main() {
+   int a = 21, b = 8, i;
+ 
+   while (b > 0) {
+      a++;
+      b--;
+   }
+ 
+   printf("Sum of a and b is : %d", a);
+   return 0;
+}
