f39817e9dd1c797adf9eb244c255069a60418ad8
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-02-22 21:48:31 -0500

    Create Structinput.c

---
+++ Structinput.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+struct Employee
+{
+char name[50];
+int emp_id;
+long phone_no;
+};
+
+int main()
+{
+struct Employee e;
+printf("Enter name : ");
+scanf("%s", &e.name);
+printf("Enter emp_id: ");
+scanf("%d", &e.emp_id);
+printf("Enter Phone Number: ");
+scanf("%ld", &e.phone_no);
+
+printf("\nEnter name : %s", e.name);
+printf("\nEnter Emp Id       : %d", e.emp_id);
+printf("\nEnter Phone Number : %ld ", e.phone_no);
+
+return 0;
+}
