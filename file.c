6e40a0a5a8df83e5317cfe6057812490aa402707
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-02-24 23:02:44 -0500

    Add files via upload

---
+++ file.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+int main()
+{
+
+FILE *fi=fopen("friends.txt","r");
+
+// Declare the variable for the data to be read from file 
+    char dataToBeRead[1000]; 
+  
+    // Open the existing file friends.txt using fopen() 
+    // in read mode using "r" attribute 
+    fi = fopen("friends.txt", "r") ; 
+      
+    // Check if this fi is null 
+    // which maybe if the file does not exist 
+    if ( fi == NULL ) 
+    { 
+        printf( "friends.txt file failed to open." ) ; 
+    } 
+    else
+    { 
+          
+        printf("The file is now opened.\n") ; 
+          
+        // Read the dataToBeRead from the file 
+        // using fgets() method 
+        while( fgets ( dataToBeRead, 50, fi ) != NULL ) 
+        { 
+          
+            // Print the dataToBeRead  
+            printf( "%s" , dataToBeRead ) ; 
+         } 
+          
+        // Closing the file using fclose() 
+        fclose(fi) ; 
+          
+        printf("Data successfully read from file friends.txt\n"); 
+        printf("The file is now closed.") ; 
+    } 
+
+return 0;
+}
