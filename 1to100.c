4a4cb83071b88d821899c4957202abe765f81bb6
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-02-29 22:49:13 -0500

    Create 1to100.c

---
+++ 1to100.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h> 
+
+  
+
+int main() 
+{ 
+
+    int i = 0; 
+begin: 
+
+    i = i + 1; 
+
+    printf("%d ", i); 
+
+  
+
+    if (i < 100) 
+
+        goto begin; 
+
+    return 0; 
+} 
