15b85bc74703868b4dd7b0da97d06bc4a314aeb8
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-05-12 18:29:01 -0400

    Create concanate_string.c

---
+++ concanate_string.c
@@ -0,0 +1,35 @@
+/*
+ ============================================================================
+ Name        : c.c
+ Author      : ronak
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main() {
+
+	int i, l1, l2;
+	char s1[100],s2[100];
+	printf("\nenter the first string : ");
+	scanf("%s",s1);
+	printf("\nenter the second string : ");
+	scanf("%s",s2);
+	l1= strlen(s1);
+	l2=strlen(s2);
+
+	for(i=0;i<=l2;i++){
+		s1[l1+i]=s2[i];
+	}
+
+	printf("\n after concanate strinfg is: %s ",s1);
+
+
+	return 0;
+}
+      
