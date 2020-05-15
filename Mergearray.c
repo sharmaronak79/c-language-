d0b622d2ef3da722ec94500399635fb8e2dd66d4
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-05-14 22:08:58 -0400

    Update Mergearray.c

--- Mergearray.c
+++ Mergearray.c
@@ -4,7 +4,7 @@ int main()
 {
  int i,h=0,k=0;
  int x[5]={1,2,3,4,5};
- int y[5]={6,7,8,9,10};
+ int y[5]={6,7,18,9,10};
  int z[10];
    printf("\n Array 1 elements are:");
    for (i = 0; i<5; i++) {
