3430b2b38e32a5d14c0d174176e2d09aefc2266d
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-05-18 17:16:57 -0400

    Create calloc__.c

---
+++ calloc__.c
@@ -0,0 +1,36 @@
+/*
+ ============================================================================
+ Name        : calloc_.c
+ Author      : ronak
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+/*https://www.youtube.com/watch?v=pTXvkLlAm38&list=PLVlQHNRLflP8IGz6OXwlV_lgHgc72aXlh&index=53*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int n;
+	int *ptr;
+	printf("Enter size of array: ");
+	scanf("%d",&n);
+
+	ptr= (int *) calloc(n,sizeof(int));
+
+	if(ptr==NULL){
+		printf("No Memory");
+	}
+	else{
+		printf("array elements are: \n");
+
+		for(int i=0;i<n;i++){
+			printf("%d\n",*(ptr+i));
+		}
+	}
+
+return 0;
+}
