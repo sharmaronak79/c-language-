b409026b1896bb44ceaf94f3ce2ce0cead919fa6
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-05-21 00:57:44 -0400

    Create ternary_operator.c

---
+++ ternary_operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a,b;
+	printf("Enter value of a : ");
+	scanf("%d",&a);
+	printf("Enter value of b : ");
+	scanf("%d",&b);
+
+	a>b? printf("a is big"):printf("b is big");  //    cond ? expreson1 : expreson2
+
+
+
+	return 0;
+}
+
