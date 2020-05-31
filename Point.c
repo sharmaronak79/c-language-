ef694a9039473ec5ac5664d5c4f3f80addbedb7d
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-05-30 23:16:02 -0400

    Create Point.c

---
+++ Point.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+int main() {
+   int a = 10, *j, **k;
+   j = &a;
+   k = &j;
+   
+   printf("\nAddress of  a = %u", &a);
+   printf("\nAddress of a = %u", j);
+   printf("\nAddress of a = %u", *k);
+   printf("\nAddress of j = %u", &j);
+   printf("\nAddress of j = %u", k);
+   printf("\nAddress of k = %u", &k);
+   printf("\nValue of j   = %u", j);
+   printf("\nValue of k   = %u", k);
+   printf("\nValue of a   = %d", a);
+   printf("\nValue of a   = %d", *(&a));
+   printf("\nValue of a   = %d", *j);
+   printf("\nValue of a   = %d", **k);
+   getch();
+}
