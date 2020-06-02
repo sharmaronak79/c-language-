6d64459df9a26ce45029f8cee866699d1c0469cf
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-06-01 23:07:45 -0400

    Create Ascending.C

---
+++ Ascending.C
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+ int j,k,s=0;
+ int x[5]={5,2,4,3,9};
+
+   printf("\n Array 1 elements are:");
+   for (k = 0; k<5; k++) {
+               printf("%d ",x[k]);
+               s=s+x[j];
+ 
+   }
+    printf("\n Sorted array are :");
+   for (k = 0; k<s; k++) {
+        for (j= 0; j<5; j++) {
+             if(x[j] ==k)
+             printf("%d",x[j]);
+                 }
+   }           
+
+    getch();
+
+
+}
