dc32db53b30f22497f6f990e71f6ae55e3ed9838
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-06-06 23:28:41 -0400

    Create Opbypointer.c

---
+++ Opbypointer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int no1, no2, *pt1, *pt2, sum;
+   
+  
+   printf(" Input the number  1: ");
+   scanf("%d", &no1);
+   printf(" Input the number 2: ");
+   scanf("%d", &no2);   
+ 
+   pt1 = &no1;
+   pt2 = &no2;
+ 
+   sum = *pt1 + *pt2;
+ 
+   printf(" The sum of the entered numbers is : %d\n\n",sum);
+ 
+   getch();
+}
