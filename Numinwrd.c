47839d2ecf6ad7e6cf4f228d19116d1f06d379ae
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-10-05 23:54:12 -0400

    Update Numinwrd.c

--- Numinwrd.c
+++ Numinwrd.c
@@ -14,7 +14,7 @@ switch(rem)
   case 0 : printf("Zero  ");break;
   case 1 : printf("One ");break;
   case 2 : printf("Two ");break;
-  case 3 : printf("Three ");break;
+  case 3 : printf("Three  ");break;
   case 4 : printf("Four ");break;
   case 5 : printf("Five ");break;
   case 6 : printf("Six ");break;
