39d99b099e9354d6ab6eb199a3f6d9644804ec1e
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-10-07 22:46:56 -0400

    Create Operation.c

---
+++ Operation.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,rem,a,b;
+	char op;
+    
+    printf("Enter Operator(+,-,*,/)");
+	scanf("%c",&op);
+    printf("Enter first numbers :");
+	scanf("%d",&a);
+	printf("Enter second numbers :");
+	scanf("%d",&b);
+	
+	switch(op)
+	{
+	case '+' : printf("Result : %d ",a+b);break;
+	case '-' : printf("Result : %d ",a-b);break;
+	case '*' : printf("Result : %d ",a*b);break;
+	case '/' : printf("Result : %d ",a/b);break;
+	default  : printf("Invalid operator ");
+	}
