73edefad0436c86a3e1cd291ed9933867b94f8d6
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-11-21 22:46:37 -0500

    Create Series2.c

---
+++ Series2.c
@@ -0,0 +1,23 @@
+Write a C program to accept a number n from user and Add n terms of the series ,
+1/2! + 2/3! + 3/4! + 4/5! + 5/6! + ………
+
+SOLUTION:
+
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,n;
+	float sum=0,fact=1;
+	printf("Enter number :");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		fact=1;
+		for(j=1;j<=i+1;j++)
+			fact=fact*j;
+	sum=sum+i/fact;
+	}
+	printf("Sum of the series 1/2! + 2/3! + 3/4! + 4/5! + 5/6! + .. is: %f",sum);
+	getch();
+}
