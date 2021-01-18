043391a97b4d51e55de858f064117103a2bffde2
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2021-01-17 23:30:53 -0500

    Update Opbypointer.c

--- Opbypointer.c
+++ Opbypointer.c
@@ -5,7 +5,7 @@ int main()
    int no1, no2, *pt1, *pt2, sum;
    
   
-   printf(" Input the Number  1: ");
+   printf(" Input the Number 1: ");
    scanf("%d", &no1);
    printf(" Input the Number 2: ");
    scanf("%d", &no2);   
