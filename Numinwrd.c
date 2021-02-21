54f606994cbe0993f9fe7d10d4fa8fb874631b24
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2021-02-20 22:53:50 -0500

    Update Numinwrd.c

--- Numinwrd.c
+++ Numinwrd.c
@@ -11,7 +11,7 @@ while(n>0)
 rem=n%10;
 switch(rem)
 {
-  case 0 : printf("Zero  ");break;
+  case 0 : printf("Zero");break;
   case 1 : printf("One ");break;
   case 2 : printf("Two ");break;
   case 3 : printf("Three  ");break;
