7e27dbd8fe9618c2461a7f286322aba251df4c12
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2021-07-09 00:31:48 +0530

    Create structure_size_.c

---
+++ structure_size_.c
@@ -0,0 +1,36 @@
+/* Here in this example we can see that without __attribute__((packed)) keyword after structure, how the  memory is wasted and occupied more memory
+   How memory is alloted to the data element of structure
+*/
+
+#include<stdio.h>
+#include<stdint.h>
+
+struct DataSet{
+	char data1;
+	 int data2;
+	char data3;
+	short data4;
+
+};
+
+int main(){
+
+	struct DataSet Data;
+	Data.data1 = 0x11;
+	Data.data2 = 0xFFFFEEEE;
+	Data.data3 = 0x22;
+	Data.data4 = 0xABCD;
+	printf("Sizeof struct variable is : %d \n",sizeof(Data));
+	printf("======================================\n");
+	printf("Address             =       Value \n");
+
+
+	uint8_t *ptr = (uint8_t*)&Data;
+	for(int i=0;i<sizeof(Data);i++){
+		printf("%p              =   %X\n",ptr, *ptr);
+		ptr++;
+
+	}
+	return 0;
+
+}
